90357c767e3e23b9d1d9c73d9a746aaff31e844f
yaroslav-sapozhnikov <64098686+yaroslav-sapozhnikov@users.noreply.github.com>
2020-09-04 00:21:17 +0300

    Add files via upload

--- tkinter1.py
+++ tkinter1.py
@@ -1,16 +1,16 @@
 from tkinter import *
 from math import sin, cos
- 
+
 root = Tk()
 
 c = Canvas(root, width=600, height=600, bg='white')
 c.pack()
 
-r = 100
+r = 200
 c.create_oval(300-r, 300-r, 300+r, 300+r)
 
 r = 10
-circle = c.create_oval(300-r, 200-r, 300+r, 200+r, fill='red')
+circle = c.create_oval(300-r, 100-r, 300+r, 100+r, fill='red')
 
 e = 0
 
@@ -20,7 +20,7 @@ def circle_move():
     global e
   
     c.move(circle, cos(e), sin(e))
-    e += 0.010
+    e += 0.005
  
     root.after(10, circle_move)
 
